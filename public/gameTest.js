ffc3b73197c2e6d2710deecce7fccbc5b43b4bbf
screwJack <gauravkh@thoughtworks.com>
2016-02-23 22:06:43 +0530

    [Madhuri/Gaurav] modify gameTest.js
    What changes have you made?
    
    * modify exsisting tests
    * added tests for setCorner and setSide functions

--- public/gameTest.js
+++ public/gameTest.js
@@ -4,17 +4,17 @@ var assert = require('chai').assert;
 
 describe('Tile', function(){
 	it('should have position', function(){
-		var tile = new game.Tile([1,2],null);
-		expect(tile.position).to.deep.equal([1,2]);
+		var tile = new game.Tile({x:1,y:2},null);
+		expect(tile.position).to.deep.equal({x:1,y:2});
 	});
 
 	it('should have owner', function(){
-		var tile = new game.Tile([2,2],'circuit');
+		var tile = new game.Tile({x:2,y:2},'circuit');
 		expect(tile.owner).to.equal('circuit');
 	});
 
 	it('should have zero hits initially', function(){
-		var tile = new game.Tile([0,0],null);
+		var tile = new game.Tile({x:0,y:0},null);
 		expect(tile.hits).to.equal(0);
 	});
 });
@@ -39,19 +39,33 @@ describe('makeTile', function(){
 		var make_tile = game.makeTiles(3);
 		expect(make_tile[0]).to.be.an('object');
 	});
+
+	it('should provide all side tiles',function(){
+		var tiles = game.makeTiles(6);
+		var sideTiles = tiles.filter(function(tile){
+			if(tile.isSide) return tile;
+		}).length;
+		expect(sideTiles).to.equal(16);
+	})
+
+	it('should provide four corner tiles',function(){
+		var tiles = game.makeTiles(6);
+		var cornerTiles = tiles.filter(function(tile){
+			return tile.isCorner;
+		}).length;
+		expect(cornerTiles).to.equal(4);
+	})
 });
 
 describe('Game',function(){
 	it('should start with two player',function(){
-		var player1 = {name : 'circuit'};
-		var player2 = {name : 'Munna_bhai'};
-		var myGame = new game.Game(player1,player2,4);
+		var players = [{name : 'circuit'},{name : 'Munna_bhai'}];
+		var myGame = new game.Game(players,4);
 		assert.ok(myGame.isStarted);
 	})
 	it('should not start if two player are not present',function(){
-		var player1 = {name:undefined};
-		var player2 = {name : 'Munna_bhai'};
-		var myGame = new game.Game(player1,player2,4);
+		var players = [{name : 'Munna_bhai'}];
+		var myGame = new game.Game(players,4);
 		assert.notOk(myGame.isStarted());
 	})
 })
