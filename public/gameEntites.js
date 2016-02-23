373defd5c56f1d1aab508aaaf37590dbe1b5707d
screwJack <gauravkh@thoughtworks.com>
2016-02-23 22:02:06 +0530

    [Madhuri/Gaurav] Modify gameEntites.js
    What changes have you made?
    
    * added isSide,isCorner,and isMiddle fields in Tile constructor
    * written setCornerTiles,setSideTiles functions
    * modify makeTiles function
    * modify game constructor
    * change the data type of tile position
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- public/gameEntites.js
+++ public/gameEntites.js
@@ -1,73 +1,74 @@
+var ld = require('lodash');
 var entities = {};
 
 entities.Tile = function(position, owner) {
 	this.position = position;
 	this.owner = owner;
 	this.hits = 0;
+	this.capacity = 4;
+	this.isCorner = false;
+	this.isSide = false;
+	this.isMiddle = true;
+}
+
+
+entities.setCornerTiles = function(tiles,length) {
+	var corners = [
+		{x:0,y:0},{x:0,y:length-1},{x:length-1,y:0},{x:length-1,y:length-1}
+	];
+	tiles.forEach(function(tile){
+		var index = ld.findIndex(corners,tile.position);
+		if(index >=0){
+			tile.capacity = 2;
+			tile.isCorner = true;
+			tile.isMiddle = false;
+		}
+	})
+
+	return entities.setSideTiles(tiles,length);
+}
+
+entities.setSideTiles = function(tiles,length) {
+	var sideTiles = [];
+	for (var i = 1; i < length-1; i++) {
+		sideTiles.push({x:0,y:i});
+		sideTiles.push({x:length-1,y:i});
+		sideTiles.push({x:i,y:0});
+		sideTiles.push({x:i,y:length-1});
+	}
+	tiles.forEach(function(tile){
+		var index = ld.findIndex(sideTiles,tile.position);
+		if(index >= 0 ) {
+			tile.capacity = 3;
+			tile.isSide = true;
+			tile.isMiddle = false;
+		}
+	});
+	return tiles;
 }
 
 entities.makeTiles = function(length) {
 	var tiles = [];
 	for (var i = 0; i < length; i++) {
 		for (var j = 0; j < length; j++) {
-			var tile = new entities.Tile([i,j], null)
+			var tile = new entities.Tile({x:i,y:j}, null);
 			tiles.push(tile);
 		}
 	}
-	return tiles;
-}
+	return entities.setCornerTiles(tiles,length);
+};
 
-// Tile.prototype = {
-// 	isCornerPosition : function(){
-// 		var corners = [0,7];
-// 		return ((this.position[0].indexOf(corners) >=0) && (this.position[1].indexOf(corners)) >= 0)
-// 		? true 
-// 		: false;
-// 	},
-
-// 	isSidePosition : function(){
-// 		var side = [1,2,3,4,5,6];
-// 		var corners = [0,7];
-
-// 	},
-
-// 	hitCount : function(){
-// 		this.hits++;
-// 	},
-
-// 	isBlast : function(position){
-
-// 	},
-
-// 	capacity : function(){
-
-// 	}
-// };
-
-entities.Game = function(player1, player2, length){
-	this.players = {player1 : player1, player2 : player2}
+entities.Game = function(players, length){
+	this.players = players;
 	this.tiles = entities.makeTiles(length);
 	this.length = length;
 }
 
 
 entities.Game.prototype = {
-
 	isStarted : function(){
-		return (this.players.player1.name && this.players.player2.name) ? true : false;
-	},
-
-	// isEnd : function(){
-
-	// },
-
-	// currentPlayer : function(){
-
-	// },
-
-	// givePositionOfTile : function(tile){
-	// 	return strirng..corner,side,middle;
-	// }
+		return this.players.length == 2;
+	}
 }
 
 
